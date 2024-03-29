9dff34a3ad5fccf34dd3f61d8af2df2bebe83a08
0xNaurmi <uracalidev@gmail.com>
2024-03-30 07:23:00 +0900

    add auth gaurd

--- src/controller/app.controller.ts
+++ src/controller/app.controller.ts
@@ -1,12 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Post, Param, Body, UseGuards, Res, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from '../service/app.service';
+import { Request, Response } from 'express';
+import { AuthGuard } from 'src/gaurd/authGaurd';
+import { Client } from 'twitter-api-sdk';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Req() req: Request): string {
     return this.appService.getHello();
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get('tweets')
+  @UseGuards(AuthGuard)
+  async getTweetsById(
+    @Param() param : any,
+    @Req() req: Request,
+    @Res() res: Response
+  ) {
+    const token = req['token'];
+
+    const client = new Client(token);
+
+    const tweet = await client.tweets.findTweetsById(param.id);
+
+    return res.json(tweet);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('tweets')
+  @UseGuards(AuthGuard)
+  async postTweet(
+    @Body() payload : any,
+    @Req() req: Request,
+    @Res() res: Response
+  ) {
+    const token = req['token'];
+
+    const client = new Client(token);
+
+    const response = await client.tweets.createTweet(payload.tweet)
+
+    return res.json(response);
   }
 }
---
+++ src/gaurd/authGaurd.ts
@@ -0,0 +1,30 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException,
+  } from '@nestjs/common';
+  import { Request } from 'express';
+  import { Reflector } from '@nestjs/core';
+  
+  @Injectable()
+  export class AuthGuard implements CanActivate {
+    constructor(private reflector: Reflector) {}
+  
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+      const request = context.switchToHttp().getRequest();
+      const token = this.extractTokenFromHeader(request);
+
+      if(token!) {
+        throw new UnauthorizedException('invalid_access_token');
+      }
+
+      request['token'] = token;
+      return true;
+    }
+  
+    private extractTokenFromHeader(request: Request): string | undefined {
+      const [type, token] = request.headers.authorization?.split(' ') ?? [];
+      return type === 'Bearer' ? token : undefined;
+    }
+  }
